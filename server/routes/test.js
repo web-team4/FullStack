b60b025b2c0bc4e717be40d136d80df8708c786a
nayonsoso <76177848+nayonsoso@users.noreply.github.com>
2021-03-21 04:41:15 +0900

    feat : 테스트 결과 수정

--- server/routes/test.js
+++ server/routes/test.js
@@ -20,6 +20,9 @@ router.get("/", function(req, res) {
 router.post("/", function(req, res) {
   let user_mbti = req.body.mbti;
   var user_id = req.session.user_id;
+  // 로그인 안했을때, session에 엠비티아이 저장 (auth 참고)
+  req.session.user_mbti = user_mbti;
+  // 로그인 했을때, db에 mbti 저장
   var sql = "UPDATE user SET user_mbti = ? WHERE user_id = ?";
   conn.query(sql,[user_mbti, user_id],function(err, rs){
     res.send({mbti:true});
