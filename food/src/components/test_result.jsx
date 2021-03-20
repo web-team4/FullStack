4a13826300c901d5b51b0abc7bbfb7d2bdf1a315
nayonsoso <76177848+nayonsoso@users.noreply.github.com>
2021-03-21 03:57:09 +0900

    feat : 테스트 수정

--- food/src/components/test_result.jsx
+++ food/src/components/test_result.jsx
@@ -1,10 +1,33 @@
 import React from "react"
+import Axios from "axios"
 import "../css/board.css"
 
-class Board extends React.Component {
+class Test_result extends React.Component {
+  state = {menu : "" ,
+    desc :"",
+    map:""}
+
+  componentDidMount(){
+    Axios.post("/test_result",{})
+        .then((res) => {
+          this.setState({
+            menu: res.data.menu,
+            desc: res.data.desc,
+            map: res.data.map})
+        })
+  }
+
   render() {
-    return <div className="all"></div>
+    let menu = this.state.menu
+    let desc = this.state.desc
+    let map = this.state.map
+    return (
+        <div className="Box">결과 페이지
+          <div className="Menu">{menu}</div>
+          <div className="Desc">{desc}</div>
+          <div className="Map">{map}</div>
+        </div>
+    )
   }
 }
-
-export default Board
+export default Test_result
