ee63c8f48dd1affea30059d32e79d8fb50d86af2
nayonsoso <76177848+nayonsoso@users.noreply.github.com>
2021-03-21 03:58:10 +0900

    feat : 테스트 수정

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,4 +1,3 @@
-//모듈
 const express = require("express")
 const bodyParser = require("body-parser")
 const router = express.Router()
@@ -19,56 +18,13 @@ router.get("/", function(req, res) {
 })
 
 router.post("/", function(req, res) {
-  console.log("/test 액션 요청")
-  const user_mbti = req.body.user_mbti // form에서 안보이게 ueser_mbti 전달
-  if (req.session.user_id) {
-    // 로그인 후 테스츠하면 데이터베이스에 결과 저장됨
-    var user_id = req.session.user_id
-    console.log("로그인 된 상태에서 테스트", user_mbti, user_id)
-    sql = "UPDATE user SET user_mbti=? WHERE user_id=?"
-    conn.query(sql, [user_mbti, user_id], function(err, result) {
-      if (err) {
-        res.send(`<script>
-                  if(confirm("테스트 결과를 저장하지 못했습니다.")){window.location.href='/'}
-                  </script>`)
-      } else {
-        console.log("인서트 성공")
-      }
-    })
-  }
-  if (user_mbti == "istj") {
-    res.send("istj")
-  } else if (user_mbti == "isfj") {
-    res.send("isfj")
-  } else if (user_mbti == "infj") {
-    res.send("infj")
-  } else if (user_mbti == "intj") {
-    res.send("intj")
-  } else if (user_mbti == "istp") {
-    res.send("istp")
-  } else if (user_mbti == "isfp") {
-    res.send("isfp")
-  } else if (user_mbti == "infp") {
-    res.send("infp")
-  } else if (user_mbti == "intp") {
-    res.send("intp")
-  } else if (user_mbti == "estp") {
-    res.send("estp")
-  } else if (user_mbti == "esfp") {
-    res.send("esfp")
-  } else if (user_mbti == "enfp") {
-    res.send("enfp")
-  } else if (user_mbti == "entp") {
-    res.send("entp")
-  } else if (user_mbti == "estj") {
-    res.send("estj")
-  } else if (user_mbti == "esfj") {
-    res.send("esfj")
-  } else if (user_mbti == "enfj") {
-    res.send("enfj")
-  } else if (user_mbti == "entj") {
-    res.send("estj")
-  }
+  let user_mbti = req.body.mbti;
+  var user_id = req.session.user_id;
+  var sql = "UPDATE user SET user_mbti = ? WHERE user_id = ?";
+  conn.query(sql,[user_mbti, user_id],function(err, rs){
+    res.send({mbti:true});
+  })
 })
 
 module.exports = router
+
